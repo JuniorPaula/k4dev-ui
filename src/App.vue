0cee992d0f2631d0e1b41940f7da6f6aa49ec838
Junior Paula <luke.junnior@icloud.com>
2023-08-02 08:43:54 -0300

    feat: implements components for the main layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
 <template>
 	<div id="app">
-		<h1>Versão Inicial</h1>
+		<Header />
+		<Menu />
+		<Content />
+		<Footer />
 	</div>
 </template>
 
 <script>
+import Menu from './components/templates/Menu.vue'
+import Header from './components/templates/Header.vue'
+import Content from './components/templates/Content.vue'
+import Footer from './components/templates/Footer.vue'
+
+
 export default {
 	name: "App",
+	components: {
+		Menu,
+		Header,
+		Content,
+		Footer
+	}
 }
+
 </script>
 
 <style>
+	* {
+		font-family: "Lato", sans-serif;
+	}
+
+	body {
+		margin: 0;
+	}
+
+	#app {
+		--webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 60px 1fr 40px;
+		grid-template-columns: 300px 1fr;
+		grid-template-areas:
+			"header header"
+			"menu content"
+			"menu footer";
+	}
 
 </style>
