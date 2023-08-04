2d936d353c2a4bfd68f967d110f01a9723274da6
Junior Paula <luke.junnior@icloud.com>
2023-08-04 08:10:07 -0300

    feat: configurate table for users components

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import './config/bootstrap'
 Vue.config.productionTip = false
 
 // Temporario
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2OTExMjMyODEsInVzZXJJZCI6NH0.az9zm6NN6Xk8ZvWufAnHm8D6AwN-uJuAsx6cqWVyS_c'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2OTExNjg0NzgsInVzZXJJZCI6NH0.PUDg2-3Z0YFgmzlclbW2fxyCKZeDqxb9xVzQ6FMz7ks'
 
 new Vue({
   store,
