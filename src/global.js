4e68e2a7723f5b77ab69dc051b5f0bcd8db2e9d3
Junior Paula <luke.junnior@icloud.com>
2023-08-27 09:55:12 -0300

    chore: add root vaiables on .env

--- src/global.js
+++ src/global.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 
 export const userKey = '__k4dev_user'
-export const baseApiUrl = 'http://localhost:8081'
+export const baseApiUrl = process.env.VUE_APP_ROOT_API
 
 export function showError(error) {
     if (error && error.response && error.response.data) {
